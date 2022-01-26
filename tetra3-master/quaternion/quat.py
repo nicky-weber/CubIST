560dc3ec192aadc99628a745e6970fbc48056d5f
Nicky Weber <nicolena.weber@gmail.com>
2022-01-26 12:15:32 -0700

    Update quat.py

--- tetra3-master/quaternion/quat.py
+++ tetra3-master/quaternion/quat.py
@@ -1,7 +1,7 @@
 """ Convert RA, DEC, and ROLL coordinates to the star camera body frame. 313 euler
     rotation. Return as a quaternion """
 
-    # Standard imports:
+# Standard imports:
 from pathlib import Path
 import logging
 import itertools
@@ -13,24 +13,26 @@ import numpy as np
 from numpy.linalg import norm
 import math
 
-#define phi alpha and delta for testing
-phi = 303.4 #degrees
-alpha = 172.368
-delta = 57.65
+#----------------- functions --------------------------
+#Create the dcm
 def dcm_generator (phi, alpha, delta):
-    phi_d = math.radians(phi)
-    alpha_d = math.radians(alpha)
-    delta_d = math.radians(delta)
-    c1 = np.array([1,0,0],[0,math.cos(delta),math.sin(delta)],[0,-1*math.sin(delta),math.cos(delta)])
-    c3 = np.array([math.cos(phi),math.sin(phi),0],[-1*math.sin(phi),math.cos(phi),0],[0,0,1])
-    c3_2 = np.array([math.cos(alpha),math.sin(alpha),0],[-1*math.sin(alpha),math.cos(alpha),0],[0,0,1])
+    phi = math.radians(phi)
+    alpha = math.radians(alpha)
+    delta = math.radians(delta)
+
+    c1 = np.array([[1,0,0],[0,math.cos(delta),math.sin(delta)],[0,-1*math.sin(delta),math.cos(delta)]])
+    c3 = np.array([[math.cos(phi),math.sin(phi),0],[-1*math.sin(phi),math.cos(phi),0],[0,0,1]])
+    c3_2 = np.array([[math.cos(alpha),math.sin(alpha),0],[-1*math.sin(alpha),math.cos(alpha),0],[0,0,1]])
 
     #multiply 313 sequence in 2 steps 3*1 then (3*1)*3_2
-    int = c1.dot(c3)
+    int = np.matmul(c3,c1)
 
-    c313 = int.dot(c3_2)
+    c313 = np.matmul(int,c3_2)
 
+    #print(c313)
     return c313
+
+#Return the quaternion
 def quat_generator (c313):
     #math conversion is easier than eigenvalue conversion. Both return the same thing
     Q = c313
@@ -39,6 +41,18 @@ def quat_generator (c313):
     q2 = (Q[2][0] - Q[0][2])/(4*q4)
     q3 = (Q[0][1] - Q[1][0])/(4*q4)
 
-    q = np.array([q1,q2,q3,q4])
-    quat = np.transpose(q)
-    return quat
+    q = np.vstack([q1,q2,q3,q4])
+    #quat = np.transpose(q)
+    print('Quaternion:')
+    print(q)
+    return q
+
+#-------------------- main ---------------------------
+
+#define phi alpha and delta for testing
+phi = 303.4 #degrees
+alpha = 172.368
+delta = 57.65
+
+c313 = dcm_generator (phi, alpha, delta)
+kumquat = quat_generator(c313)
